2c39fc57c6ddc7c4b253eb43dcf29133b949402b
Vinh.Tieng <vinh.tieng@laichi.vn>
2023-06-23 14:17:46 +0700

    update

--- extension.js
+++ extension.js
@@ -7,12 +7,12 @@ const FILE_NAME_SAVE = 'trash_file.txt';
 							Check trash file class
 --------------------------------------------------------------------------*/ 
 class CheckTrashFile {
-  constructor(root_folder, current_folder) {
+  constructor(root_folder, current_folder, ignore_folder_list = [], ignore_file_list = []) {
     this.ROOT_FOLDER = root_folder;
     this.CURRENT_FOLDER = current_folder;
-    this.IGNORE_FOLDERS = ['node_modules', '.git', '.vscode', 'dist', 'build'];
+    this.IGNORE_FOLDERS = ['node_modules', '.git', '.vscode', 'dist', 'build', 'wow'];
     this.IGNORE_FILES = ['package-lock.json', 'package.json', 'README.md', 'yarn.lock', '.gitignore', '.env'];
-    this.IMAGES_FILE_EXTENSIONS = ['.png', '.jpg', '.jpeg', '.gif', '.svg', '.ico'];
+    this.IMAGES_FILE_EXTENSIONS = ['.png', '.jpg', '.jpeg', '.gif', '.svg', '.ico', '.mp3', '.mp4', '.webm'];
     this.JS_FILE_EXTENSIONS = ['.js', '.jsx', '.tsx', '.ts'];
     this.CSS_FILE_EXTENSIONS = ['.css', '.scss', '.sass'];
     this.checking_files = [];
@@ -150,8 +150,70 @@ class CheckTrashFile {
     }
   }
 
+  shortFileType(array_file_name) {
+    let array_file_length = array_file_name.length;
+    let cssFiles = [];
+    let scssFiles = [];
+    let htmlFiles = [];
+    let jsFiles = [];
+    let imageFiles = [];
+    let otherFiles = [];
+    for(let i = 0; i < array_file_length; i++) {
+      let fileExt = path.extname(array_file_name[i]);
+      if(fileExt === '.css') {
+        cssFiles.push(array_file_name[i]);
+      } else if(fileExt === '.scss') {
+        scssFiles.push(array_file_name[i]);
+      } else if(fileExt === '.html') {
+        htmlFiles.push(array_file_name[i]);
+      } else if(fileExt === '.js') {
+        jsFiles.push(array_file_name[i]);
+      } else if(this.IMAGES_FILE_EXTENSIONS.includes(fileExt)) {
+        imageFiles.push(array_file_name[i]);
+      } else {
+        otherFiles.push(array_file_name[i]);
+      }
+    }
+    let rootFolderReplace = this.ROOT_FOLDER.replaceAll('/', '\\')
+    console.log("rootFolderReplace: ", rootFolderReplace)
+    cssFiles = cssFiles.map(item => item.replace(rootFolderReplace, ''))
+    scssFiles = scssFiles.map(item => item.replace(rootFolderReplace, ''))
+    htmlFiles = htmlFiles.map(item => item.replace(rootFolderReplace, ''))
+    jsFiles = jsFiles.map(item => item.replace(rootFolderReplace, ''))
+    imageFiles = imageFiles.map(item => item.replace(rootFolderReplace, ''))
+    otherFiles = otherFiles.map(item => item.replace(rootFolderReplace, ''))
+
+    let dataPrint = "";
+    if(cssFiles.length > 0) {
+      dataPrint += '\n\n==================== CSS FILES ====================\n';
+      dataPrint += cssFiles.join('\n');
+    }
+    if(scssFiles.length > 0) {
+      dataPrint += '\n\n==================== SCSS FILES ====================\n';
+      dataPrint += scssFiles.join('\n');
+    }
+    if(htmlFiles.length > 0) {
+      dataPrint += '\n\n==================== HTML FILES ====================\n';
+      dataPrint += htmlFiles.join('\n');
+    }
+    if(jsFiles.length > 0) {
+      dataPrint += '\n\n==================== JS FILES ====================\n';
+      dataPrint += jsFiles.join('\n');
+    }
+    if(imageFiles.length > 0) {
+      dataPrint += '\n\n==================== IMAGE FILES ====================\n';
+      dataPrint += imageFiles.join('\n');
+    }
+    if(otherFiles.length > 0) {
+      dataPrint += '\n\n==================== OTHER FILES ====================\n';
+      dataPrint += otherFiles.join('\n');
+    }
+    return dataPrint;
+  }
+
   writeFile(array_file_name = this.trash_files) {
-    fs.writeFile(this.ROOT_FOLDER + FILE_NAME_SAVE , array_file_name.join('\r\n'), function (err) {
+    let dataPrint = this.shortFileType(array_file_name);
+    fs.writeFile(this.ROOT_FOLDER + FILE_NAME_SAVE , dataPrint , function (err) {
       if (err) throw err;
     })
   }
